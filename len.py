f33d33fd45d52f1cbd47c4fe0d652f6d60160b51
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2025-08-29 17:25:02 +0530

    Added the plots without any constraint

---
+++ len.py
@@ -0,0 +1,106 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def BinaryH(a)->float:
+    """
+    Compute the binary entropy
+    """
+    a = np.asarray(a, dtype = float)
+    return np.where(np.logical_or(a==0, a==1), 0, a*np.log2(1/a) + (1-a)*np.log2(1/(1-a)) )
+
+def H(p:np.ndarray)->float:
+    """
+
+    """
+    # We'll keep the negative elemets 
+    inds = np.logical_not(p==0)
+    return -np.sum(p[inds]*np.log2(p[inds]))
+
+def bool_conv(a,b):
+    """ Returns the boolean convolution of a and b i.e a*(1-b) + b*(1-ab)"""
+    return a*(1-b) + b*(1-a)
+
+def rate_dist_weight(p):
+    """Returns the rate distortion weight for unit string, p is the relative Hamming distance"""
+    return 1-BinaryH(p)
+
+def len_gen_ab( p, qhat, alpha, rhat, s):
+    """ Returns the length of the genome given that the length of the fixed part n0 ==1 """
+    def A(rhat, s):
+        r = rhat
+        probs = np.array([1-r-s/2, s/2, s/2, r -s/2])
+        del r
+        return 2-H(probs)
+    l = (1/(1-alpha))* (A(rhat, s)*alpha + (1- 2*alpha)*rate_dist_weight(rhat)- rate_dist_weight(bool_conv(p, bool_conv(qhat,p))) )
+    return l
+
+if __name__ == "__main__":
+    p = 0.0
+    NUMPTS = 50
+    
+    alpha = 0.5 #:1 - 2*qh + 0.001
+    qmin = 1/4
+    qmax = 3*alpha/8
+    qhat = [0.25, 0.3, 0.35]
+    ls = list()
+
+    s = np.linspace(0, 1/2, NUMPTS)
+    rhat = np.linspace(0, 1/2, NUMPTS)
+    for qh in qhat:
+        l = np.zeros((s.shape[0], rhat.shape[0]))
+        for i in range(s.shape[0]):
+            for j in range(rhat.shape[0]):
+                l[i,j] = len_gen_ab(p = p, qhat= qh, alpha= alpha, rhat= rhat[j], s =s[i] )
+        ls.append(l)
+
+    xlabel = r"$\hat{r}\to$"
+    ylabel = r"$s\to$"
+    # Colormap
+    cmap = "viridis"
+    # Create figure with 3 subplots (columns)
+    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+    x_vals = rhat
+    y_vals = s
+    for i, ax in enumerate(axes):
+        im = ax.imshow(ls[i], cmap=cmap, origin='lower', aspect='auto' ,extent=[x_vals[0], x_vals[-1], y_vals[0], y_vals[-1]])
+        ax.set_aspect('equal')
+        ax.set_title(r"$\hat{q}:$" + f"{qhat[i]}"+ f' p = {p}')
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        X, Y = np.meshgrid(x_vals, y_vals)
+        mask = Y==2*X 
+        # Plot filled contours
+        cont = ax.contour(X, Y, ls[i], levels=20, cmap="viridis")
+        # Optional: plot contour lines
+        ax.plot(X[mask], Y[mask], color='red', label = r'$s=2\hat{r}$')
+        zero_mask = np.isclose(ls[i],0)
+        print(zero_mask.shape)
+        ax.scatter(X[zero_mask], Y[zero_mask], color='white') #, label = r'$s=2\hat{r}$')
+
+        nan_mask = np.isnan(ls[i])
+        ax.scatter(X[nan_mask], Y[nan_mask], color='orange') #, label = r'$s=2\hat{r}$')
+        ax.plot( rhat, rhat*(1-rhat)*2, color='peru', label = r'$\hat{r} \bigoplus\hat{r}$' )
+        print(alpha)
+        ax.axhline(y = 1 - (1-2*qhat[i])/alpha, color  = 'black', linestyle = 'dashed')
+        
+        lcpy = np.where(nan_mask, np.inf, ls[i])
+        idx = np.argmin(lcpy, axis = 0)
+        ax.plot(rhat,s[idx], marker = 'x', color = 'slategrey')
+
+        # lines = ax.contour(X, Y, ls[i], levels=10, colors="white", linewidths=0.7)
+        # ax.clabel(lines, inline=True, fontsize=8)  # label contour lines
+        # Add individual colorbar
+        cbar = fig.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+        cbar.set_label("Value")
+        ax.legend()
+    # Tight layout for spacing
+    plt.tight_layout()
+    plt.savefig(f'l_svr_alpha_{alpha:.2f}_p_{p}.png', dpi = 300, bbox_inches = 'tight')
+    plt.show()
+           #def appl_func(s,rhat):
+           #    X,Y = np.meshgrid(s, rhat, indexing='ij')
+           #    print('Grid \n', X)
+           #    print(Y)
+           #    return len_gen_ab(p, qh, alpha, Y,X )
+           #ls.append(appl_func(s, rhat))
+           #print(ls[-1])
---
+++ randomstrings.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def BinH(p):
+    return 0 if p==0 or p==1 else p*np.log2(1/p) + (1-p)*np.log2(1/(1-p))
+n0 = 64
+alpha = 0.5
+n = int(n0/(1-alpha))
+q = 0.25
+k = int(np.ceil(n*(1 - BinH(q))))
+num_fp = 2**k
+print(f'k = {k}')
+print(f'n = {n}')
+print(f'2**k = {2**k}')
+draw = 0.5
+X = np.random.choice(a= [0, 1], p=[draw, 1- draw], size = (2**k, n) )
+
+Y_target = np.random.choice(a = [0, 1], size = (n0,) )
+
+absolute_dist_lr = np.zeros((2**k,))
+absolute_dist_target_leg1 = np.zeros((2**k,)) 
+absolute_dist_target_leg2 = np.zeros((2**k,)) 
+
+lr = np.logical_xor(X[:,:n//2], X[:, n//2:])
+print(lr.shape)
+lr = np.sum(lr, axis = 1)
+print(lr.shape)
+absolute_dist_lr = lr
+ycpy = np.array([Y_target]*num_fp)
+absolute_dist_target_leg1[:num_fp] = np.sum(np.logical_xor(ycpy, X[:, :n//2]), axis =1)
+absolute_dist_target_leg2[:num_fp] = np.sum(np.logical_xor(ycpy, X[:, n//2:]), axis =1)
+
+plt.scatter(absolute_dist_lr/(n*alpha), absolute_dist_target_leg1/(n*alpha), color = 'blue')
+plt.scatter(absolute_dist_lr/(alpha*n), absolute_dist_target_leg2/(n*alpha), color = 'blue')
+plt.xlabel(r"s")
+plt.ylabel(r"$\hat{r}$")
+
+plt.show()
