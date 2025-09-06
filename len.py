b2f08690e41ef240c3bd4a29eb244eff6d5821f6
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2025-09-06 22:03:52 +0530

    Reproduced the heatmaps done by mukund and notbook for 3 cell solver

--- len.py
+++ len.py
@@ -34,9 +34,80 @@ def len_gen_ab( p, qhat, alpha, rhat, s):
     l = (1/(1-alpha))* (A(rhat, s)*alpha + (1- 2*alpha)*rate_dist_weight(rhat)- rate_dist_weight(bool_conv(p, bool_conv(qhat,p))) )
     return l
 
+def inv_bool_conv(b, c):
+    """
+        Returns a such that bool_conv(a,b) = c
+    """
+    return (1/2) * ( (c - b)/((1/2) - b) )
+
+
+class infinite_len_ab:
+
+    def __init__(self) -> None:
+        print('Initialised the infinite length class')
+
+    def BinaryH(self, a:float):
+        """ Compute the actual binary entropy"""
+        return BinaryH(a)
+
+    def H_n(self, p):
+        """ Compute the actual entropy given a list of probabilities"""
+        ps = p.T
+        ## Number of columns should be 4
+        assert ps.shape[1] == 4
+        return np.array([H(ps[i,:]) for i in range(ps.shape[0])])
+
+    def n(self, n0, alpha):
+        return n0/(1-alpha)
+
+    def len(self, q, n0, p, r, s, alpha):
+        """ 
+        Compute the length for the infinte length 
+        string genome information
+        """
+        # print( f's=  {s}', f'a= {alpha}, r = {r}')
+        n = self.n(n0 = n0, alpha=alpha)
+        C= 1- self.BinaryH(q)
+        rh =inv_bool_conv(p, r)
+        B= 1- self.BinaryH(rh)
+        probs = np.array([1-rh-s/2, s/2, s/2, rh -s/2])
+        A= 2 - self.H_n(probs)
+        return n*( alpha*A + (1- 2*alpha) * B - C)
+
+
+class finite_len_ab:
+
+    def __init__(self) -> None:
+        print('Initialised the finite length class')
+
+    def BinaryH(self, a:float, n:int):
+        return BinaryH(a) - (1/(2*n)) * np.log2(2*np.pi*n) 
+
+    def H_n(self, p, n ,t=4):
+        ps = p.T
+        assert ps.shape[1] == 4
+        entrop_actual = np.array([H(ps[i,:]) for i in range(ps.shape[0])])
+        additive =  ((1/2-t/2)*np.log2((2*np.pi*n)) + np.log2(t/2))/n
+        return entrop_actual - additive
+
+    def n(self, n0, alpha):
+        return n0/(1-alpha)
+
+    def len(self, q, n0, p, r, s, alpha):
+        n = self.n(n0 = n0, alpha=alpha)
+        C= (1- self.BinaryH(q, n))
+        rh =inv_bool_conv(p, r)
+        B= (1- self.BinaryH(rh, n))
+        probs = np.array([1-rh-s/2, s/2, s/2, rh -s/2])
+        A= (2 - self.H_n(probs, n, t = 4) ) 
+        return n*( alpha*A + (1- 2*alpha) * B - C) 
+
+
+
+
 if __name__ == "__main__":
     p = 0.0
-    NUMPTS = 50
+    NUMPTS = 1000
     
     alpha = 0.5 #:1 - 2*qh + 0.001
     qmin = 1/4
@@ -81,19 +152,28 @@ if __name__ == "__main__":
         ax.scatter(X[nan_mask], Y[nan_mask], color='orange') #, label = r'$s=2\hat{r}$')
         ax.plot( rhat, rhat*(1-rhat)*2, color='peru', label = r'$\hat{r} \bigoplus\hat{r}$' )
         print(alpha)
-        ax.axhline(y = 1 - (1-2*qhat[i])/alpha, color  = 'black', linestyle = 'dashed')
-        
+        smin = 1 - (1-2*qhat[i])/alpha
+        ax.axhline(y = smin, color  = 'black', linestyle = 'dashed')
         lcpy = np.where(nan_mask, np.inf, ls[i])
         idx = np.argmin(lcpy, axis = 0)
-        ax.plot(rhat,s[idx], marker = 'x', color = 'slategrey')
-
+        ax.scatter(rhat,s[idx], marker = 'x', color = 'slategrey', label = r'$s_{min}$ (no constraints)')
+        
+        ## Accountinf for the constrained s
+        mask = Y <=smin
+        lcpy = np.where(mask, lcpy, np.inf)
+        idx = np.argmin(lcpy, axis = 0)
+        ax.scatter(rhat,s[idx], marker = '.', color = 'magenta', label = r'$s_{min}$ (constrained)')
+        # ax.scatter(rhat,s[idx], marker = 'o', edgecolor = 'magenta', facecolor = 'none', label = r'$s_{min}$ (constrained)')
         # lines = ax.contour(X, Y, ls[i], levels=10, colors="white", linewidths=0.7)
         # ax.clabel(lines, inline=True, fontsize=8)  # label contour lines
         # Add individual colorbar
         cbar = fig.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
         cbar.set_label("Value")
-        ax.legend()
-    # Tight layout for spacing
+
+    # Tight layout for spacinghandles, labels = axes[0].get_legend_handles_labels()
+    handles, labels = axes[0].get_legend_handles_labels()
+    # One global legend outside the figure
+    fig.legend(handles, labels, loc="lower center", bbox_to_anchor=(0.5, -0.01), ncols = len(labels))
     plt.tight_layout()
     plt.savefig(f'l_svr_alpha_{alpha:.2f}_p_{p}.png', dpi = 300, bbox_inches = 'tight')
     plt.show()
