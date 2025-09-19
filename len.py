9e7a6fe4705b5b3ce69211d9acf3767f21fa0b58
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2025-09-19 16:16:43 +0530

    Added the dmin(X,Xconstr) d(given,c_constr) plot code

--- len.py
+++ len.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings
-
+from numpy.typing import ArrayLike
 def BinaryH(a)->float:
     """
     Compute the binary entropy
@@ -93,14 +93,37 @@ class infinite_len_ab:
 
 
 class finite_len_ab:
-
+    """ 
+    Finite length approximation wrapper class for the duplication AB model
+    has no data attributes just class for clubbing together the functions of the 
+    same type.
+    """
     def __init__(self) -> None:
         print('Initialised the finite length class')
 
     def BinaryH(self, a:float, n:int):
+        """
+        'Binary Entropy modification' for the finite length case
+
+        ## Inputs:
+        - a: Probability of one of the events such that p = [a, 1-a].
+        - n: Length of the string.
+        Note: This migh sometimes throw a DividebyZero warning but that's because \
+            the np.where in `BinaryH` function does not perform boolean short circuiting so it will evaluate `a==0` or\
+            `a==1` case leading to warnings, but it's still a good practice to know about the 
+            warnings so I have left them in.
+        """
         return BinaryH(a) + 1/n - (1/(2*n)) * np.log2(2*np.pi*n)
 
-    def H_n(self, p, n ,t=4):
+    def H_n(self, p:ArrayLike, n:int ,t:int=4):
+        """
+        General entropy modification for the case of finite strings.
+
+        ## Inputs:
+        - p (ArrayLike): The probability array such that `sum(p)==1` and `np.all(p>0) ==True`.
+        - n (int): The length of the string.
+        - t (int): Length of the `p`. By default it is 4.
+        """
         
         if p.ndim ==1:
             ps = p[:, np.newaxis]
@@ -113,6 +136,10 @@ class finite_len_ab:
         return entrop_actual + additive
 
     def n(self, n0, alpha):
+        """
+        Full length of the string given the length of the fixed part
+        and alpha.
+        """
         return n0/(1-alpha)
 
     def len(self, q, n0, p, r, s, alpha):
@@ -129,16 +156,12 @@ class finite_len_ab:
         """ 
         n = self.n(n0 = n0, alpha=alpha)
         assert n>0
-        # C= 1- self.H_n(np.array((q, 1-q)), n, t=2)
-        Ct= 1- self.H_n(np.array((q, 1-q)), n, t=2)
-        # C= 1- self.BinaryH(q, n ) #, t=2)
+        c_t= 1- self.H_n(np.array((q, 1-q)), n, t=2)
         rh =inv_bool_conv(p, r)
-        # B = 1 - self.BinaryH(rh, n)
-        Bt= 1- self.H_n(np.array((rh, 1-rh)), n*(1-2*alpha), t = 2) if n*(1-2*alpha) >0 else 0
-        # assert np.isclose(B,Bt) and np.isclose(C,Ct)
+        b_t= 1- self.H_n(np.array((rh, 1-rh)), n*(1-2*alpha), t = 2) if n*(1-2*alpha) >0 else 0
         probs = np.array([1-rh-s/2, s/2, s/2, rh -s/2])
         A= 2-self.H_n(probs, alpha*n, t = 4) if n*alpha>0 else 0
-        return n*( alpha*A + (1- 2*alpha) * Bt - Ct)
+        return n*( alpha*A + (1- 2*alpha) * b_t - c_t)
 
     def A(self, rh, s, n):
         probs = np.array([1-rh-s/2, s/2, s/2, rh -s/2])
@@ -149,82 +172,82 @@ class finite_len_ab:
 
 
 
-if __name__ == "__main__":
-    p = 0.0
-    NUMPTS = 1000
+# if __name__ == "__main__":
+#     p = 0.0
+#     NUMPTS = 1000
     
-    alpha = 0.5 #:1 - 2*qh + 0.001
-    qmin = 1/4
-    qmax = 3*alpha/8
-    qhat = [0.25, 0.3, 0.35]
-    ls = list()
+#     alpha = 0.5 #:1 - 2*qh + 0.001
+#     qmin = 1/4
+#     qmax = 3*alpha/8
+#     qhat = [0.25, 0.3, 0.35]
+#     ls = list()
 
-    s = np.linspace(0, 1/2, NUMPTS)
-    rhat = np.linspace(0, 1/2, NUMPTS)
-    for qh in qhat:
-        l = np.zeros((s.shape[0], rhat.shape[0]))
-        for i in range(s.shape[0]):
-            for j in range(rhat.shape[0]):
-                l[i,j] = len_gen_ab(p = p, qhat= qh, alpha= alpha, rhat= rhat[j], s =s[i] )
-        ls.append(l)
+#     s = np.linspace(0, 1/2, NUMPTS)
+#     rhat = np.linspace(0, 1/2, NUMPTS)
+#     for qh in qhat:
+#         l = np.zeros((s.shape[0], rhat.shape[0]))
+#         for i in range(s.shape[0]):
+#             for j in range(rhat.shape[0]):
+#                 l[i,j] = len_gen_ab(p = p, qhat= qh, alpha= alpha, rhat= rhat[j], s =s[i] )
+#         ls.append(l)
 
-    xlabel = r"$\hat{r}\to$"
-    ylabel = r"$s\to$"
-    # Colormap
-    cmap = "viridis"
-    # Create figure with 3 subplots (columns)
-    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
-    x_vals = rhat
-    y_vals = s
-    for i, ax in enumerate(axes):
-        im = ax.imshow(ls[i], cmap=cmap, origin='lower', aspect='auto' ,extent=[x_vals[0], x_vals[-1], y_vals[0], y_vals[-1]])
-        ax.set_aspect('equal')
-        ax.set_title(r"$\hat{q}:$" + f"{qhat[i]}"+ f' p = {p}')
-        ax.set_xlabel(xlabel)
-        ax.set_ylabel(ylabel)
-        X, Y = np.meshgrid(x_vals, y_vals)
-        mask = Y==2*X 
-        # Plot filled contours
-        cont = ax.contour(X, Y, ls[i], levels=20, cmap="viridis")
-        # Optional: plot contour lines
-        ax.plot(X[mask], Y[mask], color='red', label = r'$s=2\hat{r}$')
-        zero_mask = np.isclose(ls[i],0)
-        print(zero_mask.shape)
-        ax.scatter(X[zero_mask], Y[zero_mask], color='white') #, label = r'$s=2\hat{r}$')
+#     xlabel = r"$\hat{r}\to$"
+#     ylabel = r"$s\to$"
+#     # Colormap
+#     cmap = "viridis"
+#     # Create figure with 3 subplots (columns)
+#     fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+#     x_vals = rhat
+#     y_vals = s
+#     for i, ax in enumerate(axes):
+#         im = ax.imshow(ls[i], cmap=cmap, origin='lower', aspect='auto' ,extent=[x_vals[0], x_vals[-1], y_vals[0], y_vals[-1]])
+#         ax.set_aspect('equal')
+#         ax.set_title(r"$\hat{q}:$" + f"{qhat[i]}"+ f' p = {p}')
+#         ax.set_xlabel(xlabel)
+#         ax.set_ylabel(ylabel)
+#         X, Y = np.meshgrid(x_vals, y_vals)
+#         mask = Y==2*X 
+#         # Plot filled contours
+#         cont = ax.contour(X, Y, ls[i], levels=20, cmap="viridis")
+#         # Optional: plot contour lines
+#         ax.plot(X[mask], Y[mask], color='red', label = r'$s=2\hat{r}$')
+#         zero_mask = np.isclose(ls[i],0)
+#         print(zero_mask.shape)
+#         ax.scatter(X[zero_mask], Y[zero_mask], color='white') #, label = r'$s=2\hat{r}$')
 
-        nan_mask = np.isnan(ls[i])
-        ax.scatter(X[nan_mask], Y[nan_mask], color='orange') #, label = r'$s=2\hat{r}$')
-        ax.plot( rhat, rhat*(1-rhat)*2, color='peru', label = r'$\hat{r} \bigoplus\hat{r}$' )
-        print(alpha)
-        smin = 1 - (1-2*qhat[i])/alpha
-        ax.axhline(y = smin, color  = 'black', linestyle = 'dashed')
-        lcpy = np.where(nan_mask, np.inf, ls[i])
-        idx = np.argmin(lcpy, axis = 0)
-        ax.scatter(rhat,s[idx], marker = 'x', color = 'slategrey', label = r'$s_{min}$ (no constraints)')
+#         nan_mask = np.isnan(ls[i])
+#         ax.scatter(X[nan_mask], Y[nan_mask], color='orange') #, label = r'$s=2\hat{r}$')
+#         ax.plot( rhat, rhat*(1-rhat)*2, color='peru', label = r'$\hat{r} \bigoplus\hat{r}$' )
+#         print(alpha)
+#         smin = 1 - (1-2*qhat[i])/alpha
+#         ax.axhline(y = smin, color  = 'black', linestyle = 'dashed')
+#         lcpy = np.where(nan_mask, np.inf, ls[i])
+#         idx = np.argmin(lcpy, axis = 0)
+#         ax.scatter(rhat,s[idx], marker = 'x', color = 'slategrey', label = r'$s_{min}$ (no constraints)')
         
-        ## Accountinf for the constrained s
-        mask = Y <=smin
-        lcpy = np.where(mask, lcpy, np.inf)
-        idx = np.argmin(lcpy, axis = 0)
-        ax.scatter(rhat,s[idx], marker = '.', color = 'magenta', label = r'$s_{min}$ (constrained)')
-        # ax.scatter(rhat,s[idx], marker = 'o', edgecolor = 'magenta', facecolor = 'none', label = r'$s_{min}$ (constrained)')
-        # lines = ax.contour(X, Y, ls[i], levels=10, colors="white", linewidths=0.7)
-        # ax.clabel(lines, inline=True, fontsize=8)  # label contour lines
-        # Add individual colorbar
-        cbar = fig.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
-        cbar.set_label("Value")
+#         ## Accountinf for the constrained s
+#         mask = Y <=smin
+#         lcpy = np.where(mask, lcpy, np.inf)
+#         idx = np.argmin(lcpy, axis = 0)
+#         ax.scatter(rhat,s[idx], marker = '.', color = 'magenta', label = r'$s_{min}$ (constrained)')
+#         # ax.scatter(rhat,s[idx], marker = 'o', edgecolor = 'magenta', facecolor = 'none', label = r'$s_{min}$ (constrained)')
+#         # lines = ax.contour(X, Y, ls[i], levels=10, colors="white", linewidths=0.7)
+#         # ax.clabel(lines, inline=True, fontsize=8)  # label contour lines
+#         # Add individual colorbar
+#         cbar = fig.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+#         cbar.set_label("Value")
 
-    # Tight layout for spacinghandles, labels = axes[0].get_legend_handles_labels()
-    handles, labels = axes[0].get_legend_handles_labels()
-    # One global legend outside the figure
-    fig.legend(handles, labels, loc="lower center", bbox_to_anchor=(0.5, -0.01), ncols = len(labels))
-    plt.tight_layout()
-    plt.savefig(f'l_svr_alpha_{alpha:.2f}_p_{p}.png', dpi = 300, bbox_inches = 'tight')
-    plt.show()
-           #def appl_func(s,rhat):
-           #    X,Y = np.meshgrid(s, rhat, indexing='ij')
-           #    print('Grid \n', X)
-           #    print(Y)
-           #    return len_gen_ab(p, qh, alpha, Y,X )
-           #ls.append(appl_func(s, rhat))
-           #print(ls[-1])
+#     # Tight layout for spacinghandles, labels = axes[0].get_legend_handles_labels()
+#     handles, labels = axes[0].get_legend_handles_labels()
+#     # One global legend outside the figure
+#     fig.legend(handles, labels, loc="lower center", bbox_to_anchor=(0.5, -0.01), ncols = len(labels))
+#     plt.tight_layout()
+#     plt.savefig(f'l_svr_alpha_{alpha:.2f}_p_{p}.png', dpi = 300, bbox_inches = 'tight')
+#     plt.show()
+#            #def appl_func(s,rhat):
+#            #    X,Y = np.meshgrid(s, rhat, indexing='ij')
+#            #    print('Grid \n', X)
+#            #    print(Y)
+#            #    return len_gen_ab(p, qh, alpha, Y,X )
+#            #ls.append(appl_func(s, rhat))
+#            #print(ls[-1])
